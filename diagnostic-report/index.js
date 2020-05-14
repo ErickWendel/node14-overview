c326ee5e354c63ac9e1924e4e2e36afc68dfbb63
Erick Wendel <erick.workspace@gmail.com>
2020-05-14 12:32:56 -0300

    add example for diagnostic report
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ diagnostic-report/index.js
@@ -0,0 +1,16 @@
+// nvm use 13
+// nvm use 14
+let count = 0
+while(count < 10){
+    ++count
+    console.log('count', count)
+    setTimeout(() => {
+        Promise.reject('HO')
+        throw new Error('HEY')
+    }, 100);
+}
+process.report.reportOnUncaughtException = true
+process.report.reportOnFatalError = true
+// process.report.directory = '.'
+// process.report.writeReport('test.json')
+
